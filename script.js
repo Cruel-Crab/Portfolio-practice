c116c3ceb9d8509a2dabf8bee56263495f33d240
Mohammad <61859337+MohammadHirani@users.noreply.github.com>
2020-10-14 13:51:18 +0530

    Added JS file & enhanced responsiveness

---
+++ script.js
@@ -0,0 +1,16 @@
+// Wait till the DOM is ready.
+console.log("HiHiHi");
+$(function () {
+    var roles = ["Enthusiast", "Student", "ATHARVite"];
+    var count = 0;
+    var $typeSpan = $("#intro .content h2 span").typist({
+      text: roles[count]
+    });
+    setInterval(() => {
+      $typeSpan
+        .typistRemove(roles[count++ % roles.length].length)
+        .typistPause(2000)
+        .typistAdd(roles[count % roles.length]);
+    }, 5000);
+    });
+
