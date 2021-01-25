3b980533465861b499ec2dce2954bc973d331436
Cruel-Crab <mohammadhirani-cmpn@atharvacoe.ac.in>
2021-01-25 19:04:21 +0530

    Few improvements done

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 // Wait till the DOM is ready.
-console.log("HiHiHi");
+console.log("HiHiHi")
 $(function () {
-    var roles = ["Enthusiast", "Student", "ATHARVite"];
-    var count = 0;
-    var $typeSpan = $("#intro .content h2 span").typist({
-      text: roles[count]
-    });
-    setInterval(() => {
-      $typeSpan
-        .typistRemove(roles[count++ % roles.length].length)
-        .typistPause(2000)
-        .typistAdd(roles[count % roles.length]);
-    }, 5000);
-    });
-
+  var roles = ["Enthusiast", "Student", "ATHARVite"]
+  var count = 0
+  var $typeSpan = $("#intro .content h2 span").typist({
+    text: roles[count],
+  })
+  setInterval(() => {
+    $typeSpan
+      .typistRemove(roles[count++ % roles.length].length)
+      .typistPause(2000)
+      .typistAdd(roles[count % roles.length])
+  }, 5000)
+})
